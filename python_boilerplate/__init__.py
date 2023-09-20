886e02f43ec230df67ceb3a36167957880a07aae
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-20 15:10:58 +0100

    Clean init with .rej file

--- python_boilerplate/__init__.py
+++ python_boilerplate/__init__.py
@@ -1,4 +1,4 @@
-"""Top-level package for Python Boilerplate."""
+"""Top-level package for python_boilerplate."""
 
 __author__ = """Bryn Pickering"""
 __email__ = "17178478+brynpickering@users.noreply.github.com"
