88418cbd7247320ad8a23e4361ee49159f61e833
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2024-07-11 17:41:43 +0100

    Bump

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,5 +5,5 @@ from python_boilerplate import core
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
-    print(core.__file__ + "foo")
+    print(core.__file__ + "foobar")
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
