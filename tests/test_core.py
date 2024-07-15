4c937e227522e96f5b292521811af55a64f9fffe
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2024-07-15 10:51:01 +0100

    Add dummy test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,14 @@
 """Tests for `python_boilerplate` package."""
 
+from pathlib import Path
+
 from python_boilerplate import core
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     print(core.__file__ + "foo")
+    file = Path(__file__).parent / "foobar.txt"
+    assert not file.exists()
+    file.write_text("foobar")
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
